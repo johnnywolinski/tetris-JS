04c0bd2eced5cf422986cd6814fcf06049511a58
johnnywolinski <101757344+johnnywolinski@users.noreply.github.com>
2022-08-13 22:43:00 -0500

    added code
    added functions for movement and change rotation - changerotation is seriously broken and i know why, but I'm going to try to think of ways to fix it while I lay down

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const grid = document.querySelector('.grid');
 let squares = Array.from(document.querySelectorAll('.grid div'));
 const ScoreDisplay = document.querySelector('#score');
 const start = document.querySelector('#start');
-//L
+const colors = ['red', 'green', 'blue', 'yellow', 'brown']
+let randomColor = Math.floor(Math.random() * 4);
 const lTetromino = [
 [1, width+1, width*2+1, 2],
 [width, width+1, width+2, width*2+2],
@@ -38,25 +39,64 @@ const superTetromino = [
   [1, width + 1, width * 2 + 1, width * 3 + 1],
   [width + 1, width +2, width + 3, width + 4]
 ]
-
 const theTetrominos = [lTetromino, zTetromino, squareTetromino, superTetromino, tTetromino];
-
+//let variables
 let currentPosition = 4;
 let random = Math.floor(Math.random() *theTetrominos.length)
 let current = theTetrominos[random][0];
+let currentRotation = 0;
 
 function draw() {
   current.forEach(index => {
-    squares[currentPosition + index].classList.add('tetromino')
+    squares[currentPosition + index].style.backgroundColor = colors[randomColor];
   })
 }
-
 function undraw() {
   current.forEach(index => {
-    squares[currentPosition + index].classList.remove('tetromino')
+    squares[currentPosition + index].style.backgroundColor = 'purple'
   })
 }
+  function moveDown(){
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+  function freeze(){
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+    current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+    let random = Math.floor(Math.random() *theTetrominos.length)
+    current = theTetrominos[random][currentRotation]
+    randomColor = Math.floor(Math.random() * 4)
+    currentPosition = 4
+    draw()
+  }
+}
+function moveLeft(){
+  undraw();
+  currentPosition -= 1;
+  draw();
+  freeze();
+}
+function moveRight(){
+  undraw();
+  currentPosition += 1;
+  draw();
+  freeze();
+}
+function changeRotation(){
+  undraw();
+  currentRotation++;
+  if(currentRotation >=4){
+    currentRotation = 0;
+  }
+ current[currentRotation] += 1;
+  draw();
+  freeze();
+  console.log(currentRotation);
+}
 
+      timerId=setInterval(moveDown, 1000);
+      timerId2=setInterval(changeRotation, 4000);
 
-
-})
+  })
